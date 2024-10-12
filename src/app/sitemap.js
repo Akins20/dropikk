6057347df82b246c3a763728970a38875ee91f2d
Akins20 <ogunbiye@gmail.com>
2024-10-12 18:43:28 +0100

    created dev brancj and included site name

---
+++ src/app/sitemap.js
@@ -0,0 +1,23 @@
+
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.dropikk.com/',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.dropikk.com/features',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.dropikk.com/about',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.dropikk.com/contact',
+        lastModified: new Date(),
+      },
+      // Include other static pages as needed
+    ];
+  }
+  
